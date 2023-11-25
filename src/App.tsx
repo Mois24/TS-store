ae7cdd25284cbf3b915ff69f38533dea8b9f2771
Mois24 <artyommois024@gmail.com>
2023-11-25 09:46:17 +0100

    init: start react app 'store' with typescript and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product";
+import { Loader } from "./components/Loader";
+import { Error } from "./components/Error";
+import { Modal } from "./components/Modal";
+import { CreateProduct } from "./components/CreateProduct";
+import { useProducts } from "./hooks/products";
+
 
 function App() {
+  const {loading, error, products} = useProducts();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <Error error={error}/>}
+      {products.map(product => <Product product={product} key={product.id}/>)}
+
+      <Modal title='Create new product'>
+        <CreateProduct/>
+      </Modal>
     </div>
-  );
+  )
 }
 
 export default App;
